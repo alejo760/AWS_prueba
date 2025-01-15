a0714ba033c49e27318816f1312c10d804fe2f34
alejo760 <alejo760@gmail.com>
2025-01-15 07:43:20 -0500

    .

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ def read_root():
     return {"Hello": "World"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
